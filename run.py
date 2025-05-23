eb09fec9cb213570b47c9f471ee2c2492a44becf
Diego-Dlh <diegodelahozb@hotmail.com>
2025-05-23 01:49:18 -0500

    Initialize database tables on app startup and configure run settings

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()  # CREA TODAS LAS TABLAS según los modelos
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
