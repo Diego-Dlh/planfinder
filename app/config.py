3fb1ec1fa1fbde0532afaf4162d87b408a151e41
Diego-Dlh <diegodelahozb@hotmail.com>
2025-05-23 01:30:48 -0500

    Update configuration settings: secure SECRET_KEY, remove default database URI, and disable SQLALCHEMY_TRACK_MODIFICATIONS for production

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', '123456789')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123456@localhost:5432/planfinder')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.getenv('SECRET_KEY', 'valor_por_defecto_seguro')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Aquí no uses valor por defecto en prod
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Mejor dejar False para prod, mejora rendimiento
